0138057c6425ad2dab6187e4df8abe6b0b42545c
Ryan <ryannichpurba44@gmail.com>
2024-11-19 22:27:17 +0700

    Fixed img index.js

--- frontend/img/index.js
+++ frontend/img/index.js
@@ -3,7 +3,7 @@ import HeroBackground from "./hero_bg.png";
 import ServiceImage from "./img_service.png";
 import Testimonial1 from "./testimonial_1.png";
 import Testimonial2 from "./testimonial_2.png";
-import Testimonial3 from "./testimonial_3.png";
+import Testimonial3 from "./testimonial_3.jpeg";
 
 export {
   CarImage,
