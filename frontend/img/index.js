9ecec07146e6721580cc7c10dae0727e31fee15a
Ryan <ryannichpurba44@gmail.com>
2024-11-19 22:41:45 +0700

    Fixed login and register image

--- frontend/img/index.js
+++ frontend/img/index.js
@@ -1,4 +1,5 @@
 import CarImage from "./car.png";
+import CarAuth from "./carAuth.png";
 import HeroBackground from "./hero_bg.png";
 import ServiceImage from "./img_service.png";
 import Testimonial1 from "./testimonial_1.png";
@@ -7,6 +8,7 @@ import Testimonial3 from "./testimonial_3.jpeg";
 
 export {
   CarImage,
+  CarAuth,
   HeroBackground,
   ServiceImage,
   Testimonial1,
--- frontend/src/routes/login.lazy.jsx
+++ frontend/src/routes/login.lazy.jsx
@@ -10,6 +10,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setToken } from "../redux/slices/auth";
 import { login } from "../services/auth";
 import { useMutation } from "@tanstack/react-query";
+import { CarAuth } from "../../img";
 
 export const Route = createLazyFileRoute("/login")({
   component: Login,
@@ -74,8 +75,7 @@ function Login() {
         <Col
           md={6}
           style={{
-            backgroundImage:
-              "url('https://s3-alpha-sig.figma.com/img/293b/474b/7604a9eda79ef119b2c8196e3c8773d3?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=FZODb1Cfo9rwd9uOiWFnqdx8DgRDzG2XAOWIfxJ5G6YHLdFU8FcVi-~oHczDxVzw9SKGHMEyqVtPVO7VfQ1fZAXP3bAB8DpvU7YWjB4k8OsOPhWJ1hm3-KFgorn5WcV1hgI~m36wooGk2mZ4SEuV-mVMVs86RagT07QCYdLWQOrnSYE8FXyFB~s49m6J02aTWeqZKB86I3XT1bMPFRj3PJzMgvZ6W3QPr2esCvo~SxIbEpDLE9BRmUJ8RBN3HuJzkWkRDXjIWhff6bP3tudmJYAy1-624oE1MFx5QnLSnXYhMOvfBaM5MpbJ2E64vGpTRJXoR5j-v09qJ~~ngQlnDg__')", // Replace with your image URL
+            backgroundImage: `${CarAuth}`, // Replace with your image URL
             backgroundSize: "cover",
             backgroundPosition: "center",
             height: "100vh",
